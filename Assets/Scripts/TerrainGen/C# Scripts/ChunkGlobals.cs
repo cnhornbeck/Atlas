7d802bde9d22c22882825871b6b6d770f1427a07
Colby Hornbeck <41925332+cnhornbeck@users.noreply.github.com>
2025-01-02 14:19:51 -0600

    Tweaked Settings

--- Assets/Scripts/TerrainGen/C# Scripts/ChunkGlobals.cs
+++ Assets/Scripts/TerrainGen/C# Scripts/ChunkGlobals.cs
@@ -6,12 +6,12 @@ public class ChunkGlobals : MonoBehaviour
     public const int lodCount = 1;
 
     // Size of chunk is measured by number of edges per chunk at highest detail LOD
-    public static readonly int meshSpaceChunkSize = 512;
+    public static readonly int meshSpaceChunkSize = 64;
 
     // Size of the actual mesh in the scene
     public const float worldSpaceChunkSize = 10;
-    public const int heightMultiplier = 4;
-    public static int renderDistance = 3;
+    public const int heightMultiplier = 50;
+    public static int renderDistance = 10;
     public static int[][] triangleArrays;
     public static Vector2[][] uvArrays;
 }
